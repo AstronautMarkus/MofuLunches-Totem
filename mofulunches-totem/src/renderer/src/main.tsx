119f127fe2fae7a6369e092740777a7e7c8b4266
Marcos Reyes <astronautmarkus@gmail.com>
2024-11-10 00:25:29 -0300

    Add bootstrap + fontawesome icons

--- mofulunches-totem/src/renderer/src/main.tsx
+++ mofulunches-totem/src/renderer/src/main.tsx
@@ -1,11 +1,13 @@
-import './assets/main.css'
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import './assets/main.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
